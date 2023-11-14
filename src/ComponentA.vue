d53e8656954e90949b289d70484d681323daa952
cruldra <cruldra@gmail.com>
2023-11-14 21:39:01 +0800

    Initial commit

---
+++ src/ComponentA.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="w-full">ComponentA</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import 'uno.css';
+import ComponentA from './ComponentA.vue';
+import ComponentB from './ComponentB.vue';
+
+export {
+    ComponentA,
+    ComponentB
+};
